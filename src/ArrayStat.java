eba57f4a7a80af90ab185a7e0d6c1e067c526cc4
Brady C <bwjccoles@gmail.com>
2016-02-18 09:20:22 -0800

    Init Commit.

---
+++ src/ArrayStat.java
@@ -0,0 +1,9 @@
+/**
+ * Homework 3 for CMPT 166, Spring 2016, Trinity Western University.
+ * ArrayStat Class created according to homework specification.
+ * Allows an array of numbers, randomly generated, to be analyzed by frequency.
+ * @author Brady Coles
+ */
+public class ArrayStat {
+
+}
