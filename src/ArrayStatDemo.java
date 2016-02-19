df9f7f2c8e5edd0dea8e441f8b6a3039cfda4391
Brady C <bwjccoles@gmail.com>
2016-02-18 18:31:50 -0800

    Added program arguments to ArrayStatDemo.
    + -h -help argument
    + -l --length argument

--- src/ArrayStatDemo.java
+++ src/ArrayStatDemo.java
@@ -1,8 +1,29 @@
 
 public class ArrayStatDemo {
 
+	private static final String HELP = 
+			"Help:\n"
+			+ "-h -help: Display help.\n"
+			+ "-l <int>, -length <int>: Custom array length, default 300.\n";
 	public static void main(String[] args) {
-		ArrayStat statDemo = new ArrayStat(300);
+		int arrayLength = 300;
+		for (int i = 0; i < args.length; i++) {
+			switch (args[i]) {
+			case "-h" :
+			case "-help" :
+				System.out.println(HELP);
+				return;
+			case "-l" :
+			case "-length" : 
+				if (i + 1 < args.length) {
+					arrayLength = Integer.parseInt(args[i+1]);
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		ArrayStat statDemo = new ArrayStat(arrayLength);
 		System.out.println(statDemo);
 		statDemo.getStatistics();
 	}
